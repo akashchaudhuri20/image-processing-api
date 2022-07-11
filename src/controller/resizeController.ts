6d25307ad6b13ebe0842cbd8305bb18739038db4
Akash Chaudhuri <akashchaudhuri20@users.noreply.github.com>
2022-07-12 03:38:33 +0530

    Fixing indentations

--- src/controller/resizeController.ts
+++ src/controller/resizeController.ts
@@ -5,61 +5,61 @@ import resizeProcessor from '../processor/resizeProcessor';
 import cache from '../middleware/cacher';
 
 const resize = async (
-    req: express.Request,
-    res: express.Response
-  ): Promise<express.Response> => {
-    const originalFilePath = path.join(__dirname, '..', '..', 'original_images');
-    const newFileDirectory = path.join(__dirname, '..', '..', 'processed_images');
-  
-    const fileName = req.query.filename;
-    const width: number = parseInt(req.query.width as string);
-    const height: number = parseInt(req.query.height as string);
-  
-    // Request Validation
-    if (fileName === '') {
-      // Filename Validation
-      return res.status(501).send({ message: 'Invalid filename.' });
-    } else if (width === 0) {
-      // Width Validation
-      return res.status(502).send({ message: 'Invalid width.' });
-    } else if (height === 0) {
-      // Height Validation
-      return res.status(503).send({ message: 'Invalid height.' });
-    } 
-      // Logging Valid Input
-      const fileNameWithExtension = req.query.filename + '.jpg';
-      console.log('Filename: ' + fileNameWithExtension);
-      console.log('Width: ' + width);
-      console.log('Height: ' + height);
-  
-      let resizedImage;
-  
-      // Invoking the resizeProcessor
-      try {
+  req: express.Request,
+  res: express.Response
+): Promise<express.Response> => {
+  const originalFilePath = path.join(__dirname, '..', '..', 'original_images');
+  const newFileDirectory = path.join(__dirname, '..', '..', 'processed_images');
+
+  const fileName = req.query.filename;
+  const width: number = parseInt(req.query.width as string);
+  const height: number = parseInt(req.query.height as string);
+
+  // Request Validation
+  if (fileName === '') {
+    // Filename Validation
+    return res.status(501).send({ message: 'Invalid filename.' });
+  } else if (width === 0) {
+    // Width Validation
+    return res.status(502).send({ message: 'Invalid width.' });
+  } else if (height === 0) {
+    // Height Validation
+    return res.status(503).send({ message: 'Invalid height.' });
+  }
+  // Logging Valid Input
+  const fileNameWithExtension = req.query.filename + '.jpg';
+  console.log('Filename: ' + fileNameWithExtension);
+  console.log('Width: ' + width);
+  console.log('Height: ' + height);
+
+  let resizedImage;
+
+  // Invoking the resizeProcessor
+  try {
     resizedImage = await resizeProcessor(
       `${originalFilePath}/${fileNameWithExtension}`,
       height,
       width
     );
-      } catch (e) {
+  } catch (e) {
     return res.status(500).send({ message: 'Failed to access/resize image.' });
-      }
-      console.log('Successfully Processed Image');
-  
-      try {
-        // writing the processed data to a new file
-        const newFilePath = `${newFileDirectory}/${fileName}_${width}_${height}.jpg`;
-        await fsPromises.writeFile(newFilePath, resizedImage);
-        console.log('Successfully Saved Image');
-      } catch (e) {
-        return res.status(500).send({ message: 'Error saving the file' });
-      }
-  
-      cache.setCache(`${fileName}_${width}_${height}`, resizedImage);
-      console.log('Set to Cache');
-  
-      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-      return res.end(resizedImage);
-  };
+  }
+  console.log('Successfully Processed Image');
 
-  export default resize;
+  try {
+    // writing the processed data to a new file
+    const newFilePath = `${newFileDirectory}/${fileName}_${width}_${height}.jpg`;
+    await fsPromises.writeFile(newFilePath, resizedImage);
+    console.log('Successfully Saved Image');
+  } catch (e) {
+    return res.status(500).send({ message: 'Error saving the file' });
+  }
+
+  cache.setCache(`${fileName}_${width}_${height}`, resizedImage);
+  console.log('Set to Cache');
+
+  res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  return res.end(resizedImage);
+};
+
+export default resize;
